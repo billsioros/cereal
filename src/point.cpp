5ba2d9f8dc2d920a3611e04074f2454f51a76517
billsioros <billsioros97@gmail.com>
2019-01-24 17:59:14 +0200

    Updated point.cpp to match generated file style

--- src/point.cpp
+++ src/point.cpp
@@ -4,13 +4,21 @@
 #include <utility>
 #include <fstream>
 
-Point::Point() : x(0.0f), y(0.0f) {}
+Point::Point() : x(0.0f), y(0.0f)
+{
+}
 
-Point::Point(float x, float y) : x(x), y(y) {}
+Point::Point(float x, float y) : x(x), y(y)
+{
+}
 
-Point::Point(const Point& other) : x(other.x), y(other.y) {}
+Point::Point(const Point& other) : x(other.x), y(other.y)
+{
+}
 
-Point::Point(Point&& other) noexcept : x(std::move(other.x)), y(std::move(other.y)) {}
+Point::Point(Point&& other) noexcept : x(std::move(other.x)), y(std::move(other.y))
+{
+}
 
 Point& Point::operator=(const Point& other)
 {
