57d0518ece100ba438e2e70246dd2958ec36a78d
billsioros <billsioros97@gmail.com>
2019-01-24 02:26:37 +0200

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,37 +1,63 @@
 #!/bin/bash
 
-src="./cereal.sh"; dst="/usr/local/bin/cereal"
+cereal_src=./cereal.sh
+cereal_dst=/usr/local/bin/cereal
 
-if [ -d "$(dirname "$dst")" ]
+cereal_complete=~/.cereal_complete
+
+function confirm
+{
+    read -r -p "$1: " answer
+
+    if [[ "$answer" != [yY] ]] && [[ "$answer" != [yY][eE][sS] ]] && [ ! -z "${answer// }" ]
+    then
+        return 1
+    fi
+
+    return 0
+}
+
+if [ ! -r "$cereal_src" ] || [ ! -d "$(dirname "$cereal_dst")" ]
 then
-    sudo cp -i "$src" "$dst"
+    echo "Failed to copy '$cereal_src' to $(dirname "$cereal_dst")"; exit 1
 fi
 
-autocompletion=\
-"
-#################################################################################
-# CEREAL #
-##########
-dst=\"$dst\"
+sudo cp -i "$cereal_src" "$cereal_dst"
 
-function _cereal_
+rule=\
+"
+cereal_dst=\"$cereal_dst\"
+
+function _cereal_complete_
 {
     local cur
 
     COMPREPLY=()
     cur=\"\${COMP_WORDS[COMP_CWORD]}\"
 
-    flags=\"\$(grep -o -e '\"--[A-Za-z-]*\"' \"\$dst\" | sort --unique)\"
+    flags=\"\$(grep -soe '\"--[A-Za-z-]*\"' \"\$cereal_dst\" | sort --unique)\"
     COMPREPLY=(\$(compgen -W \"\$flags\" -- \"\$cur\"))
 }
 
-complete -F _cereal_ -o bashdefault \"\$(basename \"\$dst\")\"
-################################################################################
+complete -F _cereal_complete_ -o bashdefault \"\$(basename \"\$cereal_dst\")\"
 "
 
-read -r -p "$0: Should an autocompletion rule be added to '.bashrc': " answer
-
-if [[ "$answer" == [yY] ]] || [[ "$answer" == [yY][eE][sS] ]] || [ -z "${answer// }" ]
+entry=\
+"
+# cereal
+if [ -r $cereal_complete ]
 then
-    echo "$autocompletion" >> ~/.bashrc
+    . "$cereal_complete"
+fi
+# cereal
+"
+
+if confirm "Should an autocompletion rule be added to '.bashrc'"
+then
+    echo -n "$entry" >> ~/.bashrc
+    
+    if [ ! -w "$cereal_complete" ] || confirm "Would you like to overwrite '$cereal_complete'"
+    then
+        echo -n "$rule" > "$cereal_complete"
+    fi
 fi
